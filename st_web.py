134ae3bed70a6216b388bf7800f0434bf6662ffc
Adam Swanda <adam@deadbits.org>
2023-09-16 00:40:09 -0400

    updated web playground

--- st_web.py
+++ st_web.py
@@ -1,15 +1,26 @@
-import json
+# github.com/deadbits/vigil-llm
 import os
-import yara
+import json
+# import yara
 import requests
 import streamlit as st
 
+from streamlit_extras.badges import badge
+from streamlit_extras.stateful_button import button
 
-st.title("Prompt Analysis")
+
+st.header('Vigil - LLM security scanner')
+st.subheader('Web Playground', divider='rainbow')
 
 # Initialize session state for storing history
 if 'history' not in st.session_state:
     st.session_state['history'] = []
+
+with st.sidebar:
+    st.header('Vigil - LLM security scanner', divider='rainbow')
+    st.write('[documentation](https://vigil.deadbits.ai) | [github](https://github.com/deadbits/vigil-llm)')
+    badge(type="github", name="deadbits/vigil-llm")
+    st.divider()
 
 page = st.sidebar.radio(
     "Select a page:",
@@ -24,50 +35,51 @@ page = st.sidebar.radio(
 if page == "Prompt Analysis":
     # Text input for the user to enter the prompt
     prompt = st.text_area("Enter prompt:")
+    
+    if button("Submit", key="button1"):
+        if prompt:
+            response = requests.post(
+                "http://localhost:5000/analyze/prompt",
+                headers={"Content-Type": "application/json"},
+                data=json.dumps({"prompt": prompt})
+            )
 
-    # If the user has entered a prompt and clicks the submit button
-    if prompt and st.button("Submit"):
-        # Send POST request to server with prompt
-        response = requests.post(
-            "http://localhost:5000/analyze/prompt",
-            headers={"Content-Type": "application/json"},
-            data=json.dumps({"prompt": prompt})
-        )
+            # Check if the response was successful
+            if response.status_code == 200:
+                data = response.json()
 
-        # Check if the response was successful
-        if response.status_code == 200:
-            data = response.json()
+                # Add to history
+                st.session_state['history'].append({
+                    "timestamp": data["timestamp"],
+                    "prompt": prompt,
+                    "response": data
+                })
 
-            # Add to history
-            st.session_state['history'].append({
-                "timestamp": data["timestamp"],
-                "prompt": prompt,
-                "response": data
-            })
+                # Display the input prompt
+                st.write("**Prompt:** ", data["prompt"])
+                st.write("**Entropy:** ", data["prompt_entropy"])
+                st.write("**UUID:** ", data["uuid"])
 
-            # Display the input prompt
-            st.write("Input Prompt:", data["input_prompt"])
+                # Display messages
+                if data["messages"]:
+                    for message in data["messages"]:
+                        # the messages field holds scanners matches so raise them
+                        # as a "warning" on the UI
+                        st.warning(message,  icon="⚠️")
 
-            # Display messages
-            if data["messages"]:
-                st.write("Messages:")
-                for message in data["messages"]:
-                    st.write("- ", message)
+                # Display errors
+                if data["errors"]:
+                    st.write("Errors:")
+                    for error in data["errors"]:
+                        st.error(error)
 
-            # Display errors
-            if data["errors"]:
-                st.write("Errors:")
-                for error in data["errors"]:
-                    st.write("- ", error)
-
-            # Display results
-            st.write("Results:")
-            for scanner, details in data["results"].items():
-                st.write(f"Scanner: {scanner}")
-                for match in details["matches"]:
-                    st.write(match)
-        else:
-            st.error("Failed to get a response from the server.")
+                # Display results
+                for scanner, details in data["results"].items():
+                    st.write(f"**{scanner}**")
+                    for match in details["matches"]:
+                        st.write(match)
+            else:
+                st.error("Failed to get a response from the server.")
 
 elif page == "History":
     st.title("History")
