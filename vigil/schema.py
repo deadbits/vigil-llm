80b8b156aecae7ff33a1b8fab1dfd1d82ca0c167
Adam Swanda <adam@deadbits.org>
2023-09-15 22:40:51 -0400

    move uuid to response mode

--- vigil/schema.py
+++ vigil/schema.py
@@ -4,31 +4,33 @@ from typing import List, Optional, Dict
 from pydantic import BaseModel, Field
 
 
+class ScanModel(BaseModel):
+    prompt: str = ''
+    prompt_response: str = None
+    embedding: List[float] = []
+    results: List = []
+
+
+class ResponseModel(BaseModel):
+    status: str = ''
+    uuid: UUID = Field(default_factory=uuid4)
+    timestamp: str = ''
+    prompt: str = ''
+    prompt_response: str = None
+    prompt_entropy: float = 0.0
+    messages: List[str] = []
+    errors: List[str] = []
+    results: Dict[str, List[dict]] = {}
+
+
 class BaseScanner(ABC):
     def __init__(self, name: str = '', config_dict: Dict = {}) -> None:
         self.name = name
         self.config = config_dict
 
     @abstractmethod
-    def analyze(self, input):
+    def analyze(self, prompt: str = None, response: str = None, scan_uuid: UUID = uuid4()) -> ScanModel:
         pass
-
-
-class ScanModel(BaseModel):
-    uuid: UUID = Field(default_factory=uuid4)
-    input_prompt: str = ''
-    embedding: List[float] = []
-    results: dict = {}
-    metadata: Optional[dict] = {}
-
-
-class ResponseModel(BaseModel):
-    status: str = ''
-    timestamp: str = ''
-    input_prompt: str = ''
-    messages: List[str] = []
-    errors: List[str] = []
-    results: Dict[str, List[dict]] = {}
 
 
 class VectorMatch(BaseModel):
@@ -54,9 +56,3 @@ class SimilarityMatch(BaseModel):
     score: float = 0.0
     threshold: float = 0.0
     message: str = ''
-
-
-class ModerationMatch(BaseModel):
-    model_name: str = ''
-    category: str = ''
-    score: float = 0.0
