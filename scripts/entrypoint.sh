7e1f45a2687721dbee22c126acb1d69b68840da4
Adam Swanda <adam@deadbits.org>
2023-11-18 16:01:08 -0500

    update Docker entrypoint to use new dataloader

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-echo "Downloading datasets from huggingface ..."
-cd /app/data/datasets
-git lfs install
-git clone https://huggingface.co/datasets/deadbits/vigil-instruction-bypass-ada-002
-git clone https://huggingface.co/datasets/deadbits/vigil-jailbreak-ada-002
+echo "Loading datasets ..."
+python loader.py --config /app/conf/server.conf --dataset deadbits/vigil-instruction-bypass-ada-002
+python loader.py --config /app/conf/server.conf --dataset deadbits/vigil-jailbreak-ada-002
 
-# Load datasets into the database
-echo "Loading datasets into vector db ..."
-cd /app/vigil/utils
-python parquet2vdb.py --config /app/conf/server.conf -d /app/data/datasets/vigil-instruction-bypass-ada-002
-python parquet2vdb.py --config /app/conf/server.conf -d /app/data/datasets/vigil-jailbreak-ada-002
-
+echo " "
+echo "Starting API server ..."
 cd /app
 exec "$@"
