e591176133eb07ae6e851b8c71a0ee223efd0b69
Adam Swanda <adam@deadbits.org>
2023-11-18 15:31:09 -0500

    remove _all_ imports; not needed

--- vigil/scanners/__init__.py
+++ vigil/scanners/__init__.py
@@ -1,18 +1,3 @@
-from vigil.scanners.vectordb import VectorScanner
-from vigil.scanners.transformer import TransformerScanner
-from vigil.scanners.yara import YaraScanner
-from vigil.scanners.similarity import SimilarityScanner
-from vigil.scanners.sentiment import SentimentScanner
-
 __version__ = "0.9.7-alpha"
 __app__ = "vigil"
 __description__ = "LLM security scanner"
-
-
-__all__ = [
-    'VectorScanner',
-    'TransformerScanner',
-    'YaraScanner',
-    'SimilarityScanner',
-    'SentimentScanner'
-]
