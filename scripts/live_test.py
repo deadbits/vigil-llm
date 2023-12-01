975351ad13acc59dbf9abb1edb1328d0ee4c965d
James Hodgkinson <james@terminaloutcomes.com>
2023-12-01 17:08:27 +1000

    canary testing livetest script

--- scripts/live_test.py
+++ scripts/live_test.py
@@ -1,4 +1,6 @@
 import json
+import sys
+import time
 from loguru import logger
 import requests
 
@@ -13,12 +15,43 @@ while True:
         break
     except Exception as error:
         logger.warning("Error connecting to {}: {}", endpoint, error)
+        time.sleep(1)
 
 
-url = f"{endpoint}/analyze/prompt"
+# test an analyze-prompt request
+# url = f"{endpoint}/analyze/prompt"
+# payload = {
+#     "prompt": "Explain the difference between chalk and cheese. Please be succinct.",
+# }
+# resp = requests.post(url, json=payload)
+# logger.info(resp)
+# logger.info(json.dumps(resp.json(), indent=4))
+
+
+# test adding a canary token
+url = f"{endpoint}/canary/add"
 payload = {
     "prompt": "Explain the difference between chalk and cheese. Please be succinct.",
+    "always": True,
 }
 resp = requests.post(url, json=payload)
 logger.info(resp)
-logger.info(json.dumps(resp.json(), indent=4))
+if resp.status_code == 200:
+    logger.info(json.dumps(resp.json(), indent=4))
+else:
+    logger.error(resp.text)
+    sys.exit(1)
+
+# test checking a canary token
+url = f"{endpoint}/canary/check"
+payload = {
+    "prompt": resp.json()["result"],
+}
+logger.debug("Sending payload: {}", payload)
+resp = requests.post(url, json=payload)
+logger.info(resp)
+if resp.status_code == 200:
+    logger.info(json.dumps(resp.json(), indent=4))
+else:
+    logger.error(resp.text)
+    sys.exit(1)
